aaf7f460bdbda020a8a26d8f4439384065289fd3
Rene Cruz <renecruzATX@outlook.com>
2018-12-01 22:26:38 -0600

    Added readfile and date

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const fs = require("fs");
+const fetch = require("node-fetch");
 
 mongoose.connect("mongodb://renec:password1@ds163013.mlab.com:63013/checkpoint-backend-1");
 
@@ -12,8 +14,37 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/dateTime', (req, res) => res.send(new Date()));
+
+fs.readFile('./data.csv', 'utf8', (err, data) => {
+    if(err) throw err;
+    const dataSent = data.split('\n');
+    const dataNums = dataSent[1].split(',');
+
+    app.get('/newComments', (req, res, next) => {
+        res.send(dataNums[0]);
+    });
+    app.get('/newTasks', (req, res, next) => {
+        res.send(dataNums[1]);
+    });
+    app.get('/newOrders', (req, res, next) => {
+        res.send(dataNums[2]);
+    });
+    app.get('/tickets', (req, res, next) => {
+        res.send(dataNums[3]);
+    });
+});
+
 app.use("/messages", MessageRoutes);
 app.use("/orders", OrderRoutes);
 app.use("/tasks", TaskRoutes);
+
+app.get('/foxes', (req, res) => {
+    fetch('https://randomfox.ca/floof')
+        .then(res => res.json())
+        .then(json => {
+            res.json(json.image)
+        })
+});
 
 app.listen(3001, () => console.log('server started at localhost:3001'));
